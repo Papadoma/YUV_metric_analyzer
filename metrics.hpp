69468ad34496c12a50f7750875c4fd39ecc6f12c
Papadoma <miltiadis.alexios.papadopoulos@bristol.ac.uk>
2016-05-18 13:51:16 +0100

    Added new cpp and hpp files

---
+++ metrics.hpp
@@ -0,0 +1,24 @@
+/*
+ * metrics.hpp
+ *
+ *  Created on: 20 Apr 2016
+ *      Author: papadoma
+ */
+
+#ifndef METRICS_HPP_
+#define METRICS_HPP_
+
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+class Quality_Metrics{
+private:
+
+public:
+  Scalar getMSSIM( const Mat& i1, const Mat& i2);
+  double getPSNR(const Mat& I1, const Mat& I2);
+};
+
+
+#endif /* METRICS_HPP_ */
