b78ee0a582a5963f56435726b89085249e0fe948
papadoma <miltiadis.alexios.papadopoulos@bristol.ac.uk>
2016-05-20 18:37:51 +0100

    v1.1 Fixed bug that caused crash on selection. Also added grid.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,24 @@
+/**
+ * YUV metric analyzer
+ * Author: Miltiadis Alexios Papadopoulos
+ * Year: 2016
+ * Parts of the software come from the VQMT software.
+ */
+
+#include <string>
+#include <sstream>
+
+namespace patch
+{
+template < typename T > std::string to_string( const T& n )
+{
+	std::ostringstream stm ;
+	stm << n ;
+	return stm.str() ;
+}
+}
+
+
 #include <opencv2/opencv.hpp>
 #include <stdio.h>
 
@@ -11,10 +32,14 @@
 
 using namespace std;
 
+//Global variable - Yeah, I know Im not supposed to do this!
+char* file1;
+char* file2;
 cv::Rect mouse_selection(0,0,0,0);
 int scale = 1;
 bool mouse_click = false;
 cv::Mat mask;
+cv::Rect safe_area;
 
 
 
@@ -25,79 +50,95 @@ void CallBackFunc(int event, int x, int y, int flags, void* userdata)
 	{
 		mouse_selection = cv::Rect(0,0,0,0);
 		mouse_click = true;
-		mouse_selection.x = x/CTU_size*CTU_size;
-		mouse_selection.y = y/CTU_size*CTU_size;
+		mouse_selection.x = round(x/CTU_size)*CTU_size;
+		mouse_selection.y = round(y/CTU_size)*CTU_size;
 		mask.setTo(cv::Scalar(0,0,0));
 
 	}
 	else if  ( event == cv::EVENT_LBUTTONUP )
 	{
-		if((x - mouse_selection.x)>0 && (y - mouse_selection.y)>0){
+		if((x - mouse_selection.x)>0 && (y - mouse_selection.y)>0 && mouse_selection.x<mask.cols && mouse_selection.y<mask.rows){
 			mask.setTo(cv::Scalar(0,0,0));
-			mouse_selection.width = round((float)(x - mouse_selection.x)/CTU_size)*CTU_size;
-			mouse_selection.height = round((float)(y - mouse_selection.y)/CTU_size)*CTU_size;
+			mouse_selection.width = (int)round((float)(max(x - mouse_selection.x,CTU_size))/CTU_size)*CTU_size;
+			mouse_selection.height = (int)round((float)(max(y - mouse_selection.y,CTU_size))/CTU_size)*CTU_size;
+			mouse_selection = mouse_selection & safe_area;
 			mask(mouse_selection) = cv::Scalar(0,0,255);
+
 
 			mouse_click = false;
 			mouse_selection.x *= scale;
 			mouse_selection.y *= scale;
 			mouse_selection.width *= scale;
 			mouse_selection.height *= scale;
+
 		}
 	}
 	else if  ( event == cv::EVENT_MOUSEMOVE && mouse_click)
 	{
-		if((x - mouse_selection.x)>0 && (y - mouse_selection.y)>0){
+		if((x - mouse_selection.x)>0 && (y - mouse_selection.y)>0 && mouse_selection.x<mask.cols && mouse_selection.y<mask.rows){
 			mask.setTo(cv::Scalar(0,0,0));
 
 			//with rounding
-			mouse_selection.width = round((float)(x - mouse_selection.x)/CTU_size)*CTU_size;
-			mouse_selection.height = round((float)(y - mouse_selection.y)/CTU_size)*CTU_size;
+
+			mouse_selection.width =  (int)round((float)(max(x - mouse_selection.x,CTU_size))/CTU_size)*CTU_size;
+			mouse_selection.height = (int)round((float)(max(y - mouse_selection.y,CTU_size))/CTU_size)*CTU_size;
+			mouse_selection = mouse_selection & safe_area;
+			//cout<<mouse_selection<<endl;
 			mask(mouse_selection) += cv::Scalar(0,0,255);
 
+
 			//without rounding
+
 			mouse_selection.width = (x - mouse_selection.x);
 			mouse_selection.height = (y - mouse_selection.y);
+			mouse_selection = mouse_selection & safe_area;
+			//cout<<mouse_selection<<endl;
 			mask(mouse_selection) += cv::Scalar(255,0,0);
-
 
 		}
 	}
 }
 
 void mouse_select(cv::Mat background, cv::Rect &selection){
-	cv::Mat background_copy;
-	background.copyTo(background_copy);
-	if(background_copy.cols>1600){
-		scale = 2;
-		resize(background_copy, background_copy, cv::Size(),0.5,0.5);
+
+	//Overlay grid
+	cv::Mat grid(background.size(), CV_8UC3,cv::Scalar(0,0,0));
+	for (int col=0;col<background.cols;col+=64){	//verticals
+		line(grid, cv::Point(col,0), cv::Point(col,background.rows), cv::Scalar(255,255,255), 1, 8);
 	}
+	for (int row=0;row<background.rows;row+=64){	//horizontals
+		line(grid, cv::Point(0,row), cv::Point(background.cols,row), cv::Scalar(255,255,255), 1, 8);
+	}
+	addWeighted(background, 1, grid, -0.5, 0.0, background);
 
+	//Resize for fitting purposes
+	if(background.cols>1600){
+		scale = 2;
+		resize(background, background, cv::Size(),0.5,0.5);
+	}
+	safe_area = cv::Rect(0,0,background.cols,background.rows);
 
-	cv::namedWindow("Select Area", 1);
-	cv::setMouseCallback("Select Area", CallBackFunc, NULL);
-	mask =cv::Mat(background_copy.size(),CV_8UC3,cv::Scalar(0,0,0));
+	cv::namedWindow(string(file2)+" --- Select Area", 1);
+	cv::setMouseCallback(string(file2)+" --- Select Area", CallBackFunc, NULL);
+	mask =cv::Mat(background.size(),CV_8UC3,cv::Scalar(0,0,0));
 
 	int key;
 	do{
-		cv::imshow("Select Area", background_copy + mask);
+		cv::imshow(string(file2)+" --- Select Area", background + mask);
 		key = cv::waitKey(1);
 		//cout<<key<<endl;
 	}while(key!=1048586);	//press enter
-	if(mouse_selection.width && mouse_selection.height != 0)selection = mouse_selection;
+	if(mouse_selection.width>=64 && mouse_selection.height>=64)selection = mouse_selection;
 	std::cout<<"Area selected:"<<selection<<endl;
 }
 
 int main(int argc, char* argv[]){
-	char* file1;
-	char* file2;
+
 	char* roi_c;
 	int roi_area[4] = { 0,0,64,64};
 	cv::Size frame_size(1,1);
 	int no_frames = 1;
 	bool GUI = false;
-
-	int metric=0;
 
 	if(argc<3){
 		cout<<"Usage is -i1 <file1> -i2 <file2> -c <psnr,ssim,etc>"<<endl;
@@ -110,8 +151,6 @@ int main(int argc, char* argv[]){
 				file1 = argv[i + 1];
 			}else if(string(argv[i])=="-i2"){
 				file2 = argv[i + 1];
-			}else if(string(argv[i])=="-c"){
-				metric = atoi(argv[i + 1]);
 			}else if(string(argv[i])=="-w"){
 				frame_size.width = atoi(argv[i + 1]);
 			}else if(string(argv[i])=="-h"){
@@ -128,14 +167,16 @@ int main(int argc, char* argv[]){
 					s.erase(0, pos + string(",").length());
 					roi_area[idx++] = atoi(num_s.c_str());
 				}
+				roi_area[idx] = atoi(s.c_str());
 			}else if(string(argv[i])=="-gui"){
 				GUI = true;
-			}else if(string(argv[i])=="-help"){
-				cout<<"-i1 : input file 1"<<endl;
-				cout<<"-i2 : input file 2"<<endl;
+			}else if(string(argv[i])=="-help" || string(argv[i])=="-h"){
+				cout<<"-i1 : original sequence"<<endl;
+				cout<<"-i2 : distorted sequence"<<endl;
 				cout<<"-w : width of frame"<<endl;
 				cout<<"-h : height of frame"<<endl;
-				cout<<"-r : region to calculate the metric for, x,y,width,height"<<endl;
+				cout<<"-r : region to calculate the metric for, entered as: x,y,width,height"<<endl;
+				cout<<"-f : number of frames"<<endl;
 				cout<<"-gui : enables gui. In this case, -r is not used."<<endl;
 				cout<<"Metrics: PSNR,SSIM,PSNR-HVSM,VIFP,MS-SSIM"<<endl;
 				cout<<"PSNR-HVSM & VIFP need multiple of 8 pixels regions, MS-SSIM multiple of 16."<<endl;
@@ -179,7 +220,14 @@ int main(int argc, char* argv[]){
 			mouse_select(background,selection);
 			GUI = false;
 		}
-		imshow("Actual Area",frame_handler1.y(selection));cv::waitKey(1);
+
+		//Small debugging window
+		cv::Mat sample;
+		cvtColor(frame_handler1.ycrcb(selection),sample,CV_YCrCb2BGR);
+		string surtitle;
+		surtitle = patch::to_string(selection.width)+"x"+patch::to_string(selection.height)+" from ("+patch::to_string(selection.x)+","+patch::to_string(selection.y)+")";
+		imshow("Area selected: "+surtitle,sample);
+		cv::waitKey(1);
 
 		frame_handler1.y(selection).convertTo(roi1,CV_32F);
 		frame_handler2.y(selection).convertTo(roi2,CV_32F);
